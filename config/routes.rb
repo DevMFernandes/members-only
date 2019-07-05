9215b114dcbbbebab869dd0466a16e8146be82b4
DevMFernandes <contato@marcos-fernandes.com>
2019-07-05 16:56:34 -0300

    authentication and posts, done number 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  # get 'posts/new'
+  # get 'posts/create'
+  # get 'posts/index'
   root 'static_pages#home'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :posts, only: [:new, :create, :index]
   resources :users
 end
