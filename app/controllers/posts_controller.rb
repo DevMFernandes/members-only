e28f18940c7df9affba6c4fc8d103f334bf76a39
erib <eri-b@users.noreply.github.com>
2019-07-08 09:41:10 -0400

    added user_id col

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:body, :user_id)
   end
 
   # Confirms a logged-in user.
